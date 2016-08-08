96a4af8cbe84fa36db49b5bbebbb56306fdc5f60
Alkanoor <Alkanoor@github.com>
2016-08-08 19:24:03 +0200

    little chicken run

--- shining.cpp
+++ shining.cpp
@@ -21,11 +21,44 @@ std::vector<std::string> get_paths(const std::string& directory)
     return ret;
 }
 
-std::vector<std::pair<Polyhedra,std::string> > create_stars(const Polyhedra& p)
+std::vector<std::pair<Polyhedra,std::string> > create_stars(const Polyhedra& p, int max_spaces = 5)
 {
-    std::map<int,int> index = faces_to_map(p);
-    
+    std::map<int,int> index = faces_to_map(p.faces);
+    int n = (int)index.size();
+
     std::vector<std::pair<Polyhedra,std::string> > ret;
+    std::vector<std::vector<Graph> > graphs(n);
+    std::vector<bool> use(n);
+
+    for(int i=1;i<(1<<n);i++)
+    {
+        std::string base_hash;
+        for(int j=0;j<n;j++)
+            if(i&(1<<j))
+            {
+                use[j] = true;
+                graphs[j] = make_graphs(p.faces,j,ok,max_spaces);
+                base_hash += '1';
+            }
+            else
+            {
+                use[j] = false;
+                base_hash += '0';
+            }
+
+        std::vector<Polyhedra> polyhedrons;
+        std::vector<Point_3> points_cpy;
+        std::vector<std::vector<int> > index_cpy;
+        itere(0,use,graphs,points_cpy,index_cpy,polyhedrons);
+
+        for(unsigned int j=0;j<polyhedrons.size();j++)
+        {
+            std::ostringstream oss;
+            oss<<j;
+            ret.push_back(std::pair<Polyhedra,std::string>(polyhedrons[j],base_hash+"-"+oss.str()));
+        }
+    }
+
     return ret;
 }
 
